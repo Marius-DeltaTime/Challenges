88905966541833eb9c0f860d4574ac8ad8dfe056
Marius-DeltaTime <76968510+Marius-DeltaTime@users.noreply.github.com>
2024-07-03 20:08:56 +0200

    Initial commit

---
+++ RomanToInteger.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int RomanToInt(string s) {
+
+        Dictionary<char, int> romanDictionary = new Dictionary<char, int>
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        int totalValue = 0;
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            int currentValue = romanDictionary[s[i]];
+
+            if (i + 1 < s.Length && romanDictionary[s[i + 1]] > currentValue)
+            {
+                totalValue -= currentValue;
+            }
+            else 
+            {
+                totalValue += currentValue;
+            }
+        }
+        return totalValue;
+    }
+}
